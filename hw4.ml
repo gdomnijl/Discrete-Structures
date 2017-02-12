8eacc5406046d0a59f37ce1f66cbcca7dac4dd5b
Jinlin He <hejinlin@cs.grinnell.edu>
2017-02-12 17:17:31 -0600

    connect to MATHLAN

---
+++ hw4.ml
@@ -0,0 +1,23 @@
+(*Problem 1*)
+  datatype option = NONE | SOME of int;
+  fun optionMax(NONE, NONE) = NONE | optionMax(SOME(m),NONE) = SOME(m)
+| optionMax(NONE, SOME(m)) = SOME(m) | optionMax(SOME(m), SOME(n)) = if m >= n then SOME(m) else SOME(n);
+
+(*Problem 2 assumimg parameters are already ordered*)
+  fun setAddElem(nil, y) = [y] | setAddElem(x::xs, y) = if y < x then y::x::xs
+  else if y = x then x::xs else x::setAddElem(xs,y);
+
+    fun setUnion(nil, ys) = ys | setUnion(xs, nil) = xs |
+    setUnion(x::xs,y::ys) = if x < y then x::setUnion(xs,y::ys)
+                            else if x = y then x::setUnion(xs, ys)
+                            else y::setUnion(x::xs,ys);
+
+
+    fun setIntersection(nil, ys) = nil | setIntersection(xs,nil)=nil |setIntersection(x::xs, y::ys) = if x = y then x::setIntersection(xs,ys) else if x < y then setIntersection(xs,y::ys) else setIntersection(x::xs,ys); 
+
+    fun setMax(xs) = List.last(xs);
+
+(*Problem 3*)
+      
+(*Revision*)
+  fun flatten(nil) = [] | flatten(x::xs) = x @ flatten(xs);
